711079705bf08127bf42285d36860ee99de82a8d
Kurt Hakim <kurthakim@gmail.com>
2022-04-01 01:04:53 +0700

    added login API route

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -11,8 +11,6 @@ const handleLogin: NextApiHandler<User> = async (req, res) => {
     res.status(405).end();
     return;
   }
-  // console.log('req.body:', req.body);
-  // res.status(200).json({});
 
   const { email, password } = req.body;
   try {
