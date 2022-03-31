3e5bf01ec28ee52ad9b24a52a278b061e364adb9
Kurt Hakim <kurthakim@gmail.com>
2022-04-01 01:04:39 +0700

    added login API route

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,8 +2,16 @@ import { InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input: React.FC<InputProps> = (props) => {
-  return <input {...props} className="px-3 py-1 border rounded w-80" />;
+const Input: React.FC<InputProps> = ({ type, required, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      required={required}
+      onChange={onChange}
+      className="px-3 py-1 border rounded w-80"
+    />
+  );
 };
 
 export default Input;
