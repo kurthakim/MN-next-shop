b80642953b4f56d213a67f4afc9d071e2c5e121c
Kurt Hakim <kurthakim@gmail.com>
2022-04-02 01:49:39 +0700

    added react query

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,25 +1,10 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { fetchJson } from '../lib/api';
-import { User } from '../lib/user';
+import { useSignOut, useUser } from '../hooks/user';
 
 const NavBar: React.FC = () => {
-  const [user, setUser] = useState<User>();
-  useEffect(() => {
-    (async () => {
-      try {
-        const user = await fetchJson('/api/user');
-        setUser(user);
-      } catch (err) {
-        // not signed in
-      }
-    })();
-  }, []);
-
-  const handleSignOut = async () => {
-    await fetchJson('/api/logout');
-    setUser(undefined);
-  };
+  const user = useUser();
+  console.log('🚀 ~ file: NavBar.tsx ~ line 6 ~ user', user);
+  const signOut = useSignOut();
 
   return (
     <nav className="px-2 py-1 text-sm">
@@ -35,7 +20,7 @@ const NavBar: React.FC = () => {
             <>
               <li>{user.name}</li>
               <li>
-                <button onClick={handleSignOut}>Sign Out</button>
+                <button onClick={signOut}>Sign Out</button>
               </li>
             </>
           ) : (
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -4,31 +4,20 @@ import Button from '../components/Button';
 import Field from '../components/Field';
 import Input from '../components/Input';
 import Page from '../components/Page';
-import { fetchJson } from '../lib/api';
+import { useSignIn } from '../hooks/user';
 
 const SignInPage: React.FC = () => {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [status, setStatus] = useState({ loading: false, error: false });
+  const { signIn, signInError, signInLoading } = useSignIn();
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
-    setStatus({ loading: true, error: false });
-    try {
-      const response = await fetchJson('/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password }),
-      });
-      setStatus({ loading: false, error: false });
-      console.log(
-        '🚀 ~ file: sign-in.tsx ~ line 20 ~ consthandleSubmit:FormEventHandler<HTMLFormElement>= ~ response',
-        response
-      );
+
+    const valid = await signIn(email, password);
+    if (valid) {
       router.push('/');
-    } catch (err) {
-      setStatus({ loading: false, error: true });
     }
   };
 
@@ -51,8 +40,8 @@ const SignInPage: React.FC = () => {
             onChange={(event) => setPassword(event.target.value)}
           />
         </Field>
-        {status.error && <p className="text-red-700">Invalid credentials</p>}
-        {status.loading ? (
+        {signInError && <p className="text-red-700">Invalid credentials</p>}
+        {signInLoading ? (
           <p>Loading</p>
         ) : (
           <Button type="submit">Sign In</Button>
